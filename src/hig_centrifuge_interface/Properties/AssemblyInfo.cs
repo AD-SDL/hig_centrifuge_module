14cff30b735bc33a151085c4cbfac6747dbe37b7
AileenCleary <acleary@anl.gov>
2026-07-07 08:38:21 -0500

    Small changes. Rest node in simulate runs in 32-bit python, so run with a 32bit venv specifically

--- src/hig_centrifuge_interface/Properties/AssemblyInfo.cs
+++ src/hig_centrifuge_interface/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("hig4_node")]
+[assembly: AssemblyTitle("hig_centrifuge_interface")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("hig4_node")]
+[assembly: AssemblyProduct("hig_centrifuge_interface")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
